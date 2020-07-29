47bfcbb9e629c9e790d0473d0b45960566f7d283
unkemptArc99 <abhishekbhardwaj540@gmail.com>
2020-07-29 10:04:37 +0530

    Adding dec language
    1. Adding dec Language grammar
    2. Adding dec language confid
    3. Making changes to accommodate fileStore

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,44 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+
+interface DefLocation {
+	keyword: string,
+	filePath: string,
+	location: vscode.Location
+}
+
+function parseDecContent () {
+	let resultJson = {
+		dec : []
+	};
+	vscode.workspace.findFiles("**/*.dec").then(decFiles => {
+		decFiles.forEach ((decFile, i, decFiles) => {
+			let item = {};
+			item["file"] = decFile.pathl
+		});
+	});
+	
+}
+
+function createFileStore () {
+	let fileSystem : vscode.FileSystem = vscode.workspace.fs;
+	let workspaceRoot : string = "";
+	if (vscode.workspace.workspaceFolders) {
+	 workspaceRoot = vscode.workspace.workspaceFolders[0].name;
+	}
+	let fileStoreJsonPath : string = path.join(workspaceRoot, ".vscode/fileStore.json");
+	let fileStoreJsonUri : vscode.Uri = vscode.Uri.file(fileStoreJsonPath);
+
+
+	fileSystem.writeFile(fileStoreJsonUri, )
+
+}
+
+function refreshFileStore(event : vscode.Uri) {
+	vscode.window.showInformationMessage(event.path);
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,10 +53,16 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('uefi-code-defn.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from uefi-code-defn!');
+		if (vscode.workspace.workspaceFolders) {
+			vscode.window.showInformationMessage (vscode.workspace.workspaceFolders[0].name);
+		}
 	});
+
+	let fileWatcher : vscode.FileSystemWatcher = vscode.workspace.createFileSystemWatcher ("**/*.dec");
+	fileWatcher.onDidChange(event => refreshFileStore(event));
+	fileWatcher.onDidCreate(event => refreshFileStore(event));
+	fileWatcher.onDidDelete(event => refreshFileStore(event));
 
 	context.subscriptions.push(disposable);
 }
